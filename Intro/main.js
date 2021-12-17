ac3458bde7eebcb3eeb8f9398eff3d7f6bfa9355
nirmal <nirmalkarthikeyan1503@gmail.com>
2021-12-17 23:26:06 +0530

    first commit

---
+++ Intro/main.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            product: 'Socks',
+            image:   './assets/images/socks_green.jpg',
+            description: 'This is a description',
+            url_string: 'https://www.google.com',
+            inStock: true,
+            inventory: 100
+        }
+    }
+})
+
+
+
+
+
