c3d9975d44a3ebf3fc46469810f2e460f3ceaa8f
nirmal <nirmalkarthikeyan1503@gmail.com>
2021-12-18 10:54:38 +0530

    methods

--- Intro/main.js
+++ Intro/main.js
@@ -10,12 +10,24 @@ const app = Vue.createApp({
             onSale: true,
             details: ['50% cotton','30% wool','20% polyester'],
             variants: [
-                {id: 2234, color: 'green'},
-                {id: 2235, color: 'blue'},
+                {id: 2234, color: 'green',image: './assets/images/socks_green.jpg'},
+                {id: 2235, color: 'blue',image: './assets/images/socks_blue.jpg'},
             ],
             sizes: ['small','medium','large','extra-large','gigantic'],
+            cart: 0
         }
-    }
+    },
+    methods: {
+        addToCart(){
+            this.cart+=1
+        },
+        updateImage(variantImage){
+            this.image=variantImage
+        },
+        removeFromCart(){
+            this.cart-=1
+        }
+    },
 })
 
 
