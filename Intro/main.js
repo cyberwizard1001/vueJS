5a15b666ca79ec861e532d2c6f040e3c46001520
nirmal <nirmalkarthikeyan1503@gmail.com>
2021-12-18 11:59:37 +0530

    class and style binding

--- Intro/main.js
+++ Intro/main.js
@@ -6,7 +6,7 @@ const app = Vue.createApp({
             description: 'This is a description',
             url_string: 'https://www.google.com',
             inStock: true,
-            inventory: 100,
+            inventory: 2,
             onSale: true,
             details: ['50% cotton','30% wool','20% polyester'],
             variants: [
@@ -14,18 +14,34 @@ const app = Vue.createApp({
                 {id: 2235, color: 'blue',image: './assets/images/socks_blue.jpg'},
             ],
             sizes: ['small','medium','large','extra-large','gigantic'],
-            cart: 0
+            cart: 0,
+            styles: {
+                color: 'red',
+                fontSize: '14px'
+            }
         }
     },
     methods: {
         addToCart(){
             this.cart+=1
+            this.inventory-=1
+            this.checkStock()
         },
         updateImage(variantImage){
             this.image=variantImage
         },
         removeFromCart(){
             this.cart-=1
+            this.inventory+=1
+            this.checkStock()
+        },
+        checkStock(){
+            if(this.inventory==0){
+                this.inStock=false
+            }
+            else{
+                this.inStock=true
+            }
         }
     },
 })
