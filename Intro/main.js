a2b5102c231e4a083d5ee96407c629c934de4334
nirmal <nirmalkarthikeyan1503@gmail.com>
2021-12-18 10:26:08 +0530

    added cart and add to cart

--- Intro/main.js
+++ Intro/main.js
@@ -8,7 +8,12 @@ const app = Vue.createApp({
             inStock: true,
             inventory: 100,
             onSale: true,
-            details: ['50% cotton','30% wool','20% polyester']
+            details: ['50% cotton','30% wool','20% polyester'],
+            variants: [
+                {id: 2234, color: 'green'},
+                {id: 2235, color: 'blue'},
+            ],
+            sizes: ['small','medium','large','extra-large','gigantic'],
         }
     }
 })
