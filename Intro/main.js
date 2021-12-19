41a8fafd8a8b42b75812f4f8de33f6dad02d3a24
nirmal <nirmalkarthikeyan1503@gmail.com>
2021-12-19 09:13:10 +0530

    Added reviews

--- Intro/main.js
+++ Intro/main.js
@@ -1,52 +1,13 @@
 const app = Vue.createApp({
-    data(){
-        return{
-            product: 'Socks',
-            image:   './assets/images/socks_green.jpg',
-            description: 'This is a description',
-            url_string: 'https://www.google.com',
-            inStock: true,
-            inventory: 2,
-            onSale: true,
-            details: ['50% cotton','30% wool','20% polyester'],
-            variants: [
-                {id: 2234, color: 'green',image: './assets/images/socks_green.jpg'},
-                {id: 2235, color: 'blue',image: './assets/images/socks_blue.jpg'},
-            ],
-            sizes: ['small','medium','large','extra-large','gigantic'],
-            cart: 0,
-            styles: {
-                color: 'red',
-                fontSize: '14px'
-            }
+    data() {
+        return {
+            cart: [],
+            premium: true
         }
     },
     methods: {
-        addToCart(){
-            this.cart+=1
-            this.inventory-=1
-            this.checkStock()
-        },
-        updateImage(variantImage){
-            this.image=variantImage
-        },
-        removeFromCart(){
-            this.cart-=1
-            this.inventory+=1
-            this.checkStock()
-        },
-        checkStock(){
-            if(this.inventory==0){
-                this.inStock=false
-            }
-            else{
-                this.inStock=true
-            }
+        updateCart(id){
+            this.cart.push(id)
         }
-    },
+    }
 })
-
-
-
-
-
